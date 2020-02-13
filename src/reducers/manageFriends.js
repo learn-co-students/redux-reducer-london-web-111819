95fcfa1db29c43020cd6b503996dd917110564f0
badogan <basri.dogan.71@gmail.com>
2020-02-13 09:33:18 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(stateReceived, action){
+
+    state = {
+        friends: []
+    }
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            state.friends = [...stateReceived.friends, action.friend]
+          return state;
+        case 'REMOVE_FRIEND':
+            state.friends = stateReceived.friends.filter(friend=>friend.id!==action.friend.id)
+            return state;
+        default:
+            return stateReceived;
+    }
 }
