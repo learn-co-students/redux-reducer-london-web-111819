06682ae906938795436d69b171f6e34f130e81ba
badogan <basri.dogan.71@gmail.com>
2020-04-15 10:46:57 +0100

    revising

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,11 +1,25 @@
-export function manageFriends(state={friends:[]}, action){
-
-    switch (action.type) {
-        case 'ADD_FRIEND':
-            return ({...state, friends:[...state.friends, action.friend]});
-        case 'REMOVE_FRIEND':
-            return ({...state, friends:[state.friends.filter(friend=>friend.id!==action.friend.id)]})
-        default:
-            return state;
-    }
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {
+        ...state,
+        friends: [
+          ...state.friends,
+          {
+            name: action.friend.name,
+            hometown: action.friend.hometown,
+            id: action.friend.id
+          }
+        ]
+      };
+    case "REMOVE_FRIEND":
+      return {
+        ...state,
+        friends: [
+          state.friends.filter(friend => friend.id !== action.friend.id)
+        ]
+      };
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,11 +1,13 @@
+export function managePresents(state = { numberOfPresents: 0 }, action) {
 
-export function managePresents(state, action){
-
-    state = {
-        numberOfPresents: 0
+    switch (action.type) {
+        case "INCREASE":
+            return {
+                ...state,
+                numberOfPresents: state.numberOfPresents + 1
+            };
+        default:
+            return state;
     }
 
-    if (action.type === 'INCREASE') {state.numberOfPresents = state.numberOfPresents + 1}
-
-    return state
 }
